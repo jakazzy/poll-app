25168a4403a15f4c194717e779815feaca2cfea0
jida <jakazzy@gmail.com>
2019-05-26 08:48:37 +0000

    edit event handler for onclick to the button event in square

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class Square extends React.Component {
       return (
         <button 
         className="square"
-        onClick= { this.setState({value:'X'})}>
+        onClick= { () => this.setState({value:'X'})}>
           {this.state.value}
         </button>
       );
