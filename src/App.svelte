decacd1878cd7c099c21e130912d3f7ca17f4f74
jakazzy <jakazzy@gmail.com>
2020-07-06 18:57:17 +0000

    set up poll project

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+</script>
+
+<Header/>
+<main>
+<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsam inventore necessitatibus, vel, eum culpa voluptatem cupiditate, delectus error ad reiciendis tempora omnis sed eaque provident repudiandae in obcaecati. Vero? </p>
+</main>
+<Footer/>
+<style>
+	main{
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
