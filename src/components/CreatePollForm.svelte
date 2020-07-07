7bf5fc1a0b3a0950524a0899f295181397059e00
jakazzy <jakazzy@gmail.com>
2020-07-07 07:00:43 +0000

    validate form component

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,24 +1,54 @@
 <script>
 import Button from '../shared/Button.svelte'
 let fields = { question: '', answerA: '', answerB: ''}
+let errors = { question: '', answerA: '', answerB: ''}
+let valid=false
 
 const handleSubmit = ()=>{
     console.log(fields);
-    
+    // validate  question
+    if(fields.question.trim().length < 5){
+      valid = false
+      errors.question='Question must be at least 5 characters long'
+    } else {
+      errors.question=''
+    }
+    // validate answer
+    if(fields.answerA.trim().length < 1){
+      valid = false
+      errors.answerA='Answer A cannot be empty'
+    } else {
+      errors.answerA=''
+    }
+
+    // validate answer
+    if(fields.answerB.trim().length < 1){
+      valid = false
+      errors.answerB='Answer A cannot be empty'
+    } else {
+      errors.answerB=''
+    }
+}
+//  add new poll
+if (valid){
+  console.log('valid', fields)
 }
 </script>
 <form on:submit|preventDefault ={handleSubmit}>
 <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.queston}>
+    <div class="error">{errors.question}</div>
 </div>
 <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{errors.answerA}</div>
 </div>
 <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{errors.answerB}</div>
 </div>
 <Button type='secondary'>Add Poll</Button>
 </form>
